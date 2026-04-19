2b6e327f3b9a3a65c2089660b50fa0cc5f85c6b2
cejnik <chejnma@gmail.com>
2026-04-19 13:54:41 +0200

    add forms type

--- symfony/MovieTracker/src/Form/MovieType.php
+++ symfony/MovieTracker/src/Form/MovieType.php
@@ -6,18 +6,22 @@ use App\Entity\Movie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class MovieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('director')
-            ->add('genre')
-            ->add('releaseYear')
-            ->add('rating')
-            ->add('watched')
+            ->add('title', TextType::class, ['label' => 'Movie title'])
+            ->add('director', TextType::class, ['label' => 'Director'])
+            ->add('genre', ChoiceType::class, ['label' => 'Genre', 'choices' => ['Action' => 'Action', 'Comedy' => 'Comedy', 'Drama' => 'Drama', 'Horror' => 'Horror', 'Sci-fi' => 'Sci-fi', 'Thriller' => 'Thriller', 'Animation' => 'Animation', 'Documentary' => 'Documentary'], 'placeholder' => 'Choose genre'])
+            ->add('releaseYear', IntegerType::class, ['label' => 'Release Year', 'attr' => ['min' => 1888, 'max' => 2035]])
+            ->add('rating', IntegerType::class, ['label' => 'Rating 1-10', 'attr' => ['min' => 1, 'max' => 10]])
+            ->add('watched', CheckboxType::class, ['label' => 'Already watched', 'required' => false])
         ;
     }
 

b92dcf33183c4c9a8aa3f3bffbf65a13744bbbcb
cejnik <chejnma@gmail.com>
2026-04-23 17:28:38 +0200

    add: new reservation

--- symfony/RoomBooking/src/Controller/ReservationController.php
+++ symfony/RoomBooking/src/Controller/ReservationController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
+use App\Form\ReservationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ReservationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ReservationController extends AbstractController
 {
@@ -15,6 +19,24 @@ final class ReservationController extends AbstractController
         $reservations = $reservationRepository->findBy([], ['id' => 'ASC']);
         return $this->render('reservation/index.html.twig', [
             'reservations' => $reservations,
+        ]);
+    }
+
+    #[Route('/reservation/new', name: 'new_reservation')]
+    public function new_reservation(Request $request, EntityManagerInterface $entityManagerInterface): Response
+    {
+        $reservation = new Reservation();
+        $form = $this->createForm(ReservationType::class, $reservation);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $reservation->setStatus('confirmed');
+            $reservation->setCreatedAt(new \DateTimeImmutable());
+            $entityManagerInterface->persist($reservation);
+            $entityManagerInterface->flush();
+            return $this->redirectToRoute('index_reservation');
+        }
+        return $this->render('reservation/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
